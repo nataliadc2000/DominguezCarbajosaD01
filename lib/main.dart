bd5b44d66de1296176b5bd6bf7483f93259f7f85
Natalia Domínguez <ndcarbajosa@gmail.com>
2023-12-19 19:31:30 +0100

    eliminacion d los componentes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
-// import 'package:ejemploflutter/widgets/cflutterlogo.dart';
-import 'package:ejemploflutter/widgets/contador.dart';
-import 'package:ejemploflutter/widgets/intro.dart';
 import 'package:ejemploflutter/widgets/pageMain.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    body: SingleChildScrollView(
-      child: Column(
-        children: [Nav(), Intro(), PantallaContador()],
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(textTheme).copyWith(
+          bodyMedium: GoogleFonts.poppins(textStyle: textTheme.bodyMedium),
+        ),
       ),
-    ),
-  )));
+      debugShowCheckedModeBanner: false,
+      title: 'Ejercicio 01',
+      home: const Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Nav(),
+              PantallaContador(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
